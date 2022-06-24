079001d37b16370e056bd0fa448fa45d427e0a62
soufiane.elkharmo <soufiane.elkharmo>
2022-06-24 11:32:39 +0200

    mise a jour

--- drawing/lire_file.c
+++ drawing/lire_file.c
@@ -7,9 +7,9 @@
 int lire_file(Liste *my_liste, char *name_file, int records, int type_file)
 {
 
-  FILE *file;
-  file = fopen(name_file, "r");
-  if (file == NULL)
+  FILE *fid;
+  fid = fopen(name_file, "r");
+  if (fid == NULL)
   {
     printf("Error opening file.\n");
     return 0;
@@ -19,7 +19,7 @@ int lire_file(Liste *my_liste, char *name_file, int records, int type_file)
   {
     do
     {
-      fscanf(file,
+      fscanf(fid,
              "%30[^,] , %30[^,],  %d \n",
              my_liste[records].ville1,
              my_liste[records].ville2,
@@ -27,7 +27,7 @@ int lire_file(Liste *my_liste, char *name_file, int records, int type_file)
 
       records++;
       my_liste[records].indice = records;
-    } while (!feof(file));
+    } while (!feof(fid));
   }
 
   // Lecture 2 int
@@ -35,57 +35,61 @@ int lire_file(Liste *my_liste, char *name_file, int records, int type_file)
   {
     do
     {
-      fscanf(file,
+      fscanf(fid,
              "%d, %d\n",
              &my_liste[records].dist,
              &my_liste[records].indice);
 
       records++;
-    } while (!feof(file));
+    } while (!feof(fid));
   }
   // Lecture 1string et 2 int
   if (type_file == 2)
   {
     do
     {
-      fscanf(file,
+      fscanf(fid,
              "%30[^,] , %d,  %d \n",
              my_liste[records].ville1,
              &my_liste[records].dist,
              &my_liste[records].indice);
 
-    } while (!feof(file));
+    } while (!feof(fid));
   }
-  fclose(file);
+  fclose(fid);
   return records;
 }
 
-int lire_file_perimetre(char* name_file, int *x, int *y){
+int lire_file_perimetre(char *name_file, int *x, int *y)
+{
 
-    char *import_database;
-    char table[TMP_MAX];
-    char *x_char;
-    char *y_char;
-    int i=0;
-    FILE *fid;
-    fid=fopen(name_file, "r");
-    if(fid){
-        while(1) {
-            import_database=fgets(table,999, fid);
-            if( import_database==NULL )
-                break;
-            x_char = strtok(table," ");
-            y_char = strtok(NULL,"\n");
-            x[i]=atoi(x_char);
-            y[i]=atoi(y_char);
-            printf("%d %d\n", x[i], y[i]);
-            i++;
-        }
-    }else
-        printf("\nErreur d’ouverture de fichier\n");
+  char *import_database;
+  char table[TMP_MAX];
+  char *x_char;
+  char *y_char;
+  int i = 0;
+  FILE *fid;
+  fid = fopen(name_file, "r");
+  if (fid)
+  {
+    while (1)
+    {
+      import_database = fgets(table, 999, fid);
+      if (import_database == NULL)
+        break;
+      x_char = strtok(table, " ");
+      y_char = strtok(NULL, "\n");
+      x[i] = atoi(x_char);
+      y[i] = atoi(y_char);
+      printf("%d %d\n", x[i], y[i]);
+      i++;
+    }
+  }
+  else
+    printf("\nErreur d’ouverture de fichier\n");
 
-    fclose(fid);
-    fflush(stdin);
+  fclose(fid);
+  fflush(stdin);
 
-    return i;
+  return i;
 }
